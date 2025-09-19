9e344bbf2637e357566e77a8ae884bede3326015
Karen Araujo <barbiekarenalves@outlook.com>
2025-09-19 17:29:34 -0300

    projeto nivel do heroi

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("desafio de projeto")
+let nomeDoHeroi = "Kate"
+let experiencia = 4600
+let nivel = ""
+
+if (experiencia <= 1000) {
+    nivel = "Ferro"
+} else if (experiencia <= 2000) {
+    nivel = "Bronze"
+} else if (experiencia <= 5000) {
+    nivel = "Prata"
+} else if (experiencia <= 7000) {
+    nivel = "Ouro"
+} else if (experiencia <= 8000) {
+    nivel = "Platina"
+} else if (experiencia <= 9000) {
+    nivel = "Ascendente"
+} else if (experiencia <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+console.log("O Herói de nome " + nomeDoHeroi + " está no nivel:" + nivel)
